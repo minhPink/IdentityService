156bf88708497be8c72652ea96102f09cc85ed20
xminh <kirito14042003@gmail.com>
2025-03-07 23:58:27 +0700

    get and update user

--- src/main/java/com/minh/dev/controller/UserController.java
+++ src/main/java/com/minh/dev/controller/UserController.java
@@ -1,20 +1,38 @@
 package com.minh.dev.controller;
 
 import com.minh.dev.dto.request.UserCreationRequest;
+import com.minh.dev.dto.request.UserUpdateRequest;
 import com.minh.dev.entity.User;
 import com.minh.dev.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
     }
+
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId,@RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
 }
--- src/main/java/com/minh/dev/service/UserService.java
+++ src/main/java/com/minh/dev/service/UserService.java
@@ -1,10 +1,13 @@
 package com.minh.dev.service;
 
 import com.minh.dev.dto.request.UserCreationRequest;
+import com.minh.dev.dto.request.UserUpdateRequest;
 import com.minh.dev.entity.User;
 import com.minh.dev.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +24,24 @@ public class UserService {
         user.setDob(request.getDob());
 
         return  userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String id, UserUpdateRequest request) {
+        User user = getUser(id);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
     }
 }
